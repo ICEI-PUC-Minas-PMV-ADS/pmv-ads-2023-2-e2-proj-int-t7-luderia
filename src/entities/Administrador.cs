c158e2a15ad9c991111247aeb8452b1697a658e6
FarleyDeSouza <nabucodonosor.ia.2023@gmail.com>
2023-10-18 23:03:09 -0300

    Adicionando classes de entidades

---
+++ src/entities/Administrador.cs
@@ -0,0 +1,16 @@
+class Administrador {
+    private string nomeAdmin;
+    private string email;
+
+
+    public Administrador() {}
+
+    public Administrador(string nomeAdmin, string email) {
+        this.nomeAdmin = nomeAdmin;
+        this.email = email;
+    }
+
+    public bool atualizaCatalogo() {
+        
+    }
+}
---
+++ src/entities/Cliente.cs
@@ -0,0 +1,28 @@
+class Cliente {
+
+    public string nomeCliente;
+    public string endereco;
+    public string email;
+
+
+    public Cliente() {}
+
+    public Cliente(string nomeCliente, string endereco, string email) {
+        this.nomeCliente = nomeCliente;
+        this.endereco = endereco;
+        this.email = email;
+    }
+
+
+    public void cadastro() {
+
+    }
+
+    public void login() {
+
+    }
+
+    public void atualizarPerfil() {
+        
+    }
+}
